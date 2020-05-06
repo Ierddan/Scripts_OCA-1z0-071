a33dae7f70640db8cbccbccbf6e2842617e947ca
Costa <ierddan.de.o.costa@accenture.com>
2020-05-06 14:28:57 -0300

    Terminido do Script 13 - Subqueries

--- Scripts_DQL/Select_13_subquery.sql
+++ Scripts_DQL/Select_13_subquery.sql
@@ -18,3 +18,29 @@ FROM   hr.employees
 WHERE  SALARY > ALL (SELECT SALARY 
                  FROM   hr.employees 
                  WHERE  Upper(JOB_ID) = 'SA_REP');
+                 
+
+--Subquery contendo valores nulls
+--Se o conjunto de subconsultas contiver um valor nulo, a consulta não recuperará nenhum registro
+
+SELECT employee_id, 
+       first_name, 
+       last_name, 
+       salary 
+FROM   hr.employees 
+WHERE  employee_id NOT IN (SELECT DISTINCT manager_id 
+                           FROM   hr.employees); 
+                           
+--Exists e Not Exists
+
+SELECT * 
+FROM   hr.departments dept 
+WHERE  EXISTS (SELECT dept.department_id 
+               FROM   hr.employees EMP 
+               WHERE  emp.department_id = dept.department_id) 
+
+
+
+
+
+
